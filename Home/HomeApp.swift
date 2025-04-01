9cc24a4b7939392f206a6f57c1a83d8f6d676575
Your Name <your.email@example.com>
2025-03-31 21:32:24 -0400

    Initial Commit

---
+++ Home/HomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeApp.swift
+//  Home
+//
+//  Created by UNKNOWN on 3/31/25.
+//
+
+import SwiftUI
+
+@main
+struct HomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HomeTests/HomeTests.swift
@@ -0,0 +1,17 @@
+//
+//  HomeTests.swift
+//  HomeTests
+//
+//  Created by UNKNOWN on 3/31/25.
+//
+
+import Testing
+@testable import Home
+
+struct HomeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
